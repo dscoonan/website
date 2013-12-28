b268905aa4c5ae400e717435e37231a00b5c02db
dsc995 <dsc995@gmail.com>
2013-12-27 22:37:03 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/*$(document).ready(function() {
+    $(".jake img, .zoey img").mouseenter(function() {
+        $(this).slideUp(500);
+    });
+    $(".jake img, .zoey img").mouseleave(function() {
+        $(this).slideDown(500);
+    });
+    });
+    */
